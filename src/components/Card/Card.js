e16edff17e3b48b8f1b2236bef0a8ea3c4b4df62
Ali <anix1@kent.edu>
2022-10-14 21:14:18 -0600

    add functionality to show single movie and main page along with CSS hover effects

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({title, rating, posterImage, id, updateView}) => {
+const Card = ({title, rating, posterImage, showSingleMovie, id}) => {
     return (
         <div className = 'card'>
-            <img className = 'mini-poster' alt='movie poster' src= {posterImage} onClick = {() => updateView(id)}/>
+            <img className = 'mini-poster' alt='movie poster' src= {posterImage} onClick = {() => showSingleMovie(id)}/>
             <p>{ title }</p>
             <p>Rating: { rating }</p>
         </div>
