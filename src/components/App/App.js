0ce51f97e8e2b32487e32b96343ea0b6af3ee9c6
Nicole Forseth <forseth.nico@gmail.com>
2022-10-21 17:20:00 -0600

    Fix styling for single movie page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,7 +34,6 @@ class App extends Component {
         {!this.state.errorMessage &&
         <Switch>
           <Route path = '/:id' render = {({match}) => {
-          //const selectedMovie = this.state.movies.find(movie => movie.id === parseInt(match.params.id))
           return <SingleMovie id = {match.params.id}/>
           }}/> 
           <Route path ='/' render = {() => <Movies movies = {this.state.movies}/>}/>
