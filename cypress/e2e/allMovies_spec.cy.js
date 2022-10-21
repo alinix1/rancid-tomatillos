d74126f78a4e14658d470c33fb2c0060d346d12f
Nicole Forseth <forseth.nico@gmail.com>
2022-10-20 23:13:49 -0600

    Add server error tests for single movie

--- cypress/e2e/allMovies_spec.cy.js
+++ cypress/e2e/allMovies_spec.cy.js
@@ -1,50 +1,57 @@
 describe("Main Movie Page", () => {
-
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit("http://localhost:3000");
   });
 
-  it('A user should be able to visit http://localhost:3000 and see the application title', () => {
-    cy.get('h1').contains('🍿 Rancid Tomatillos 🎬')
+  it("should be able to visit http://localhost:3000 and see the application title", () => {
+    cy.get("h1").contains("🍿 Rancid Tomatillos 🎬");
   });
 
-  it('A user should be able to visit http://localhost:3000 and view a collection of movies', () => {
-    cy.get('.movies-container').should('be.visible')
-    cy.get('.mini-poster').should('have.length', 40)
-  })
-
-   it('A user should be able to see movies, each with their poster, title and rating', () => {
-    cy.get('.mini-poster').should('be.visible')
-    cy.contains('.card-title', '2067')
-    cy.contains('.card-rating', 5)
-  })
-
-  it('A user should not be able to see movie details for a single movie', () => {
-    cy.get('.movies-container').should('be.visible')
-      .get('.single-movie.container').should('not.exist')
+  it("should be able to visit http://localhost:3000 and view a collection of movies", () => {
+    cy.get(".movies-container").should("be.visible");
+    cy.get(".mini-poster").should("have.length", 40);
   });
 
-  it('A user should be able to click on a movie, and be shown additional details about that movie', () => {
-    cy.get('.movies-container').children().contains('2067').click()
-    cy.url().should('include', '/528085')
-    cy.contains('h2', '2067')
+  it("should be able to see movies, each with their poster, title and rating", () => {
+    cy.get(".mini-poster").should("be.visible");
+    cy.contains(".card-title", "2067");
+    cy.contains(".card-rating", 5);
   });
 
-  it('should show an error message when movie content returns not found from API fetch', () => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-      statusCode: 404,
-      body: {message: 'Something went wrong, please try again!'}
-    })
-    cy.contains('.error-message', 'Something went wrong, please try again!')
+  it("should not be able to see movie details for a single movie", () => {
+    cy.get(".movies-container")
+      .should("be.visible")
+      .get(".single-movie.container")
+      .should("not.exist");
   });
 
-  it('should show an error message when the internal server is down', () => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-      statusCode: 500,
-      body: {message: 'Something went wrong, please try again!'}
-    })
-    cy.contains('.error-message', 'Something went wrong, please try again!')
+  it("should be able to click on a movie, and be shown additional details about that movie", () => {
+    cy.get(".movies-container").children().contains("2067").click();
+    cy.url().should("include", "/528085");
+    cy.contains("h2", "2067");
   });
-  
-}); 
-   
+
+  it("should show an error message when movie content returns not found from API fetch", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        statusCode: 404,
+        body: { message: "Something went wrong, please try again!" },
+      }
+    );
+    cy.contains(".error-message", "Something went wrong, please try again!");
+  });
+
+  it("should show an error message when the internal server is down", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        statusCode: 500,
+        body: { message: "Something went wrong, please try again!" },
+      }
+    );
+    cy.contains(".error-message", "Something went wrong, please try again!");
+  });
+});
--- cypress/e2e/singleMovie_spec.cy.js
+++ cypress/e2e/singleMovie_spec.cy.js
@@ -1,33 +1,118 @@
-describe('Single Movie Page', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000/')
-      cy.get('.movies-container').children().contains('After We Collided').click()
-    });
+describe("Single Movie Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
 
-    it('A user should see the movie details for a specific movie', () => {
-      cy.url().should('include', '/613504')
-        cy.contains('h2', 'After We Collided')
-        cy.get('.single-movie-backdrop').should('have.attr', 'src', "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg")
-        cy.contains('p', 'Overview: Tessa finds herself struggling with her complicated relationship with Hardin; she faces a dilemma that could change their lives forever.')
-        cy.contains('p', 'Release Date: 2020-09-02')
-        cy.contains('p', 'Genres: RomanceDrama')
-        cy.contains('p','Runtime: 105 minutes')
-    });
+  it("should be able to view the page title", () => {
+    cy.get("h1").contains("🍿 Rancid Tomatillos 🎬");
+  });
 
-    it('A user should be able to click the X button on the movie details page and navigate back to home', () => {
-      cy.get('.button').click()
-        .go('back')
-        .go('forward')
-    });
+  it("should see the movie details for a specific movie", () => {
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.url().should("include", "/613504");
+    cy.contains("h2", "After We Collided");
+    cy.get(".single-movie-backdrop").should(
+      "have.attr",
+      "src",
+      "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg"
+    );
+    cy.contains(
+      "p",
+      "Overview: Tessa finds herself struggling with her complicated relationship with Hardin; she faces a dilemma that could change their lives forever."
+    );
+    cy.contains("p", "Release Date: 2020-09-02");
+    cy.contains("p", "Genres: RomanceDrama");
+    cy.contains("p", "Runtime: 105 minutes");
+  });
 
-    it('A user should be able to see the movie trailer carousel to view different movies', () => {
-      cy.get('.all-swiper-movies').children(). should('be.visible')
-    });
+  it("should be able to click the X button on the movie details page and navigate back to home", () => {
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.get(".button").click().go("back").go("forward");
+  });
 
-    it('A user should be able to click on the swiper buttons to change the movie', () => {
-      cy.get('.swiper-button-next').click({multiple : true})
-    });
+  it("should be able to see the movie trailer carousel to view different movies", () => {
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.get(".all-swiper-movies").children().should("be.visible");
+  });
 
-    it('')
+  it("should be able to click through the swiper buttons to change the movie", () => {
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.get(".swiper-button-next").click({ multiple: true });
+  });
 
-})
+  it("should show an error message when individual movie details returns not found from API fetch", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/613504",
+      {
+        statusCode: 404,
+        body: { message: "Something went wrong, please try again!" },
+      }
+    );
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.contains(".error-message", "Something went wrong, please try again!");
+  });
+
+  it("should show an error message when individual movie trailers return not found from API fetch", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/613504/videos",
+      {
+        statusCode: 404,
+        body: { message: "Something went wrong, please try again!" },
+      }
+    );
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.contains(".error-message", "Something went wrong, please try again!");
+  });
+
+  it("should show an error message when the internal server is down for a movie", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/613504",
+      {
+        statusCode: 500,
+        body: { message: "Something went wrong, please try again!" },
+      }
+    );
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.contains(".error-message", "Something went wrong, please try again!");
+  });
+
+  it("should show an error message when the internal server is down for an individual movies trailers", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/613504/videos",
+      {
+        statusCode: 500,
+        body: { message: "Something went wrong, please try again!" },
+      }
+    );
+    cy.get(".movies-container")
+      .children()
+      .contains("After We Collided")
+      .click();
+    cy.contains(".error-message", "Something went wrong, please try again!");
+  });
+});
