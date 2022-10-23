f793ad0a57ad31c47146f9a0218327c0d384a456
Nicole Forseth <forseth.nico@gmail.com>
2022-10-23 16:43:29 -0600

    Prettier all files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,49 +1,63 @@
-import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
-import loading from '../../assets/refresh.png';
-import { fetchAllData } from '../../apiCalls';
-import SingleMovie from '../SingleMovie/SingleMovie';
-import Movies from '../Movies/Movies';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import loading from "../../assets/refresh.png";
+import { fetchAllData } from "../../apiCalls";
+import SingleMovie from "../SingleMovie/SingleMovie";
+import Movies from "../Movies/Movies";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { 
+    this.state = {
       movies: [],
-      errorMessage: ''
-    }
+      errorMessage: "",
+    };
   }
 
   componentDidMount() {
-    fetchAllData('/movies')
-    .then(data => this.setState({movies: data.movies}))
-    .catch(error => {
-      this.setState({
-        ...this.state,
-        errorMessage: 'Something went wrong, please try again!'})
-    })
+    fetchAllData("/movies")
+      .then((data) => this.setState({ movies: data.movies }))
+      .catch((error) => {
+        this.setState({
+          ...this.state,
+          errorMessage: "Something went wrong, please try again!",
+        });
+      });
   }
 
   render() {
     return (
-      <main className = 'App'>
+      <main className="App">
         <nav>
-          <h1 className ='main-title'>🍿  Rancid Tomatillos  🎬</h1>
+          <h1 className="main-title">🍿 Rancid Tomatillos 🎬</h1>
         </nav>
-        {!this.state.errorMessage &&
-        <Switch>
-          <Route path = '/:id' render = {({match}) => {
-          return <SingleMovie id = {match.params.id}/>
-          }}/> 
-          <Route path ='/' render = {() => <Movies movies = {this.state.movies}/>}/>
-        </Switch>}
-        {this.state.errorMessage && <h4 className= 'error-message'>{this.state.errorMessage}</h4>}
-        {!this.state.errorMessage && !this.state.movies.length && <div><img src = {loading} alt='loading' className='loading-image'/><h4>Loading...</h4></div>}
+        {!this.state.errorMessage && (
+          <Switch>
+            <Route
+              path="/:id"
+              render={({ match }) => {
+                return <SingleMovie id={match.params.id} />;
+              }}
+            />
+            <Route
+              path="/"
+              render={() => <Movies movies={this.state.movies} />}
+            />
+          </Switch>
+        )}
+        {this.state.errorMessage && (
+          <h4 className="error-message">{this.state.errorMessage}</h4>
+        )}
+        {!this.state.errorMessage && !this.state.movies.length && (
+          <div>
+            <img src={loading} alt="loading" className="loading-image" />
+            <h4>Loading...</h4>
+          </div>
+        )}
       </main>
-    )
+    );
   }
-
 }
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import './Card.css'
-import tomatillo from '../../assets/tomatillo.png';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Card.css";
+import tomatillo from "../../assets/tomatillo.png";
 
-const Card = ({title, rating, poster, id}) => {
-    return (
-        <NavLink to = {`/${id}`}>
-            <div className = 'card' id = {id}>
-                <img className = 'mini-poster' alt='movie poster' src= {poster} />
-                <p className = 'card-title'>{ title }</p>
-                <div className= 'rating-container'>
-                <p className = 'card-rating'><img src = {tomatillo} className = 'tomatillo-image' alt='tomatillo'/> Rating: { rating } </p>
-                </div>
-            </div>
-        </NavLink>    
-    )
-}
-
+const Card = ({ title, rating, poster, id }) => {
+  return (
+    <NavLink to={`/${id}`}>
+      <div className="card" id={id}>
+        <img className="mini-poster" alt="movie poster" src={poster} />
+        <p className="card-title">{title}</p>
+        <div className="rating-container">
+          <p className="card-rating">
+            <img src={tomatillo} className="tomatillo-image" alt="tomatillo" />
+            Rating: {rating}
+          </p>
+        </div>
+      </div>
+    </NavLink>
+  );
+};
 
 export default Card;
